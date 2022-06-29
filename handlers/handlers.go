7e3286a8080c5e349719aa89eb417ce04dca40d9
Dusan Ivanisevic <dusan.ivanisevic@brightmarbles.io>
2022-06-29 15:51:53 +0200

    winners handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,13 +14,13 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	year := req.URL.Query().Get("year")
 	res.Header().Set("Content-Type", "application/json")
-	if year == "" {		
-	winners, err := data.ListAllJSON()
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	res.Write(winners)
+	if year == "" {
+		winners, err := data.ListAllJSON()
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Write(winners)
 	} else {
 		winners, err := data.ListAllByYear(year)
 		if err != nil {
@@ -29,14 +29,14 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 		}
 		res.Write(winners)
 	}
-	
+
 }
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 	accessToken := req.Header.Get("X-ACCESS-TOKEN")
 	isValidToken := data.IsAccessTokenValid(accessToken)
-	
+
 	if !isValidToken {
 		res.WriteHeader(http.StatusUnauthorized)
 		return
@@ -46,11 +46,23 @@ func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusUnprocessableEntity)
 		return
 	}
-	
+
 	res.WriteHeader(http.StatusCreated)
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
 func WinnersHandler(res http.ResponseWriter, req *http.Request) {
-
+	switch req.Method {
+		case http.MethodGet:{
+			ListWinners(res, req)
+			break
+		}
+		case http.MethodPost: {
+			AddNewWinner(res, req)
+			break
+		}
+		default: {
+			res.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	}
 }
